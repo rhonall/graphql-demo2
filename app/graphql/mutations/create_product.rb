ac7f1966d153b291634e888eb14d5e03768718d9
Rhona Lam <rainie.gi.lam@gmail.com>
2023-07-10 13:08:31 +1000

    Fix error

--- app/graphql/mutations/create_product.rb
+++ app/graphql/mutations/create_product.rb
@@ -1,8 +1,5 @@
 module Mutations
   class CreateProduct < BaseMutation
-    class LinkedProvider < Types::BaseInputObject
-    end
-
     # arguments passed to the `resolve` method
     argument :name, String, required: true
     argument :provider_id, ID, required: true
@@ -16,10 +13,10 @@ module Mutations
         name: name,
         provider: provider
       )
-    rescue ActiveRecord::RecordInvalid => e
-      GraphQL::ExecutionError.new("Invalid input: #{e.record.errors.full_messages.join(', ')}")
     rescue ActiveRecord::RecordNotFound => e
       GraphQL::ExecutionError.new("Record not found: #{e}")
+    rescue ActiveRecord::RecordInvalid => e
+      GraphQL::ExecutionError.new("Invalid input: #{e.record.errors.full_messages.join(', ')}")
     end
   end
 end
